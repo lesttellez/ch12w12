a29159a2f58da817311928b3fffcb4a2876852fb
Celeste T <celestet@Celestes-MacBook-Air.local>
2022-08-22 11:56:22 -0700

    trying to fix code

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -12,27 +12,27 @@ VALUES ("Legal");
 INSERT INTO role (title, salary, department_id)
 VALUES ("Sales Lead", 100000, 1);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Lead Engineer", 110000, 2);
+VALUES ("Lead Engineer", 150000, 2);
 INSERT INTO role (title, salary, department_id)
 VALUES ("Software Engineer", 120000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Accountant", 115000, 3);
+VALUES ("Accountant", 125000, 3);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Legal Team Lead", 1200000, 4);
+VALUES ("Legal Team Lead", 250000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Brianna", "Rene", 1, null);
+VALUES ("John", "Doe", 1, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Leon", "King", 2, 1);
+VALUES ("Mike", "Chan", 2, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Kate", "Miller", 3, 3);
+VALUES ("Ashley", "Rodriguez", 3, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Walter", "Jones", 4, 3);
+VALUES ("Kevin", "Tupik", 4, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tom", "Bryant", 5, null);
+VALUES ("Malia", "Brown", 5, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Guy", "Hoop", 2, 2);
+VALUES ("Sarah", "Lourd", 2, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("David", "Brown", 4, 7);
+VALUES ("Tom", "Allen", 4, 7);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Daisy", "Martin", 1, null);
+VALUES ("Christian", "Eckenrode", 1, 2);
